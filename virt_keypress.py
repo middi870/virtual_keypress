35b924911d8379ba840320d6a50b9b14f38c239c
middi870 <midwayworld19@gmail.com>
2026-03-11 19:39:21 +0530

    added code

--- virt_keypress.py
+++ virt_keypress.py
@@ -1,1 +1,47 @@
-# Your program goes here
+import uinput
+import time
+
+# Keys we will use
+events = (
+    uinput.KEY_LEFTMETA,
+    uinput.KEY_Q,
+    uinput.KEY_E,
+    uinput.KEY_C,
+    uinput.KEY_H,
+    uinput.KEY_O,
+    uinput.KEY_I,
+    uinput.KEY_SPACE,
+    uinput.KEY_ENTER,
+    uinput.KEY_APOSTROPHE,
+)
+
+device = uinput.Device(events)
+
+time.sleep(2)  # time to switch to desktop
+
+# ---- open terminal (Super + Q) ----
+device.emit(uinput.KEY_LEFTMETA, 1)
+device.emit(uinput.KEY_Q, 1)
+device.emit(uinput.KEY_Q, 0)
+device.emit(uinput.KEY_LEFTMETA, 0)
+
+time.sleep(1)
+
+# ---- type echo "hi" ----
+sequence = [
+    uinput.KEY_E,
+    uinput.KEY_C,
+    uinput.KEY_H,
+    uinput.KEY_O,
+    uinput.KEY_SPACE,
+    uinput.KEY_APOSTROPHE,
+    uinput.KEY_H,
+    uinput.KEY_I,
+    uinput.KEY_APOSTROPHE,
+]
+
+for key in sequence:
+    device.emit_click(key)
+    time.sleep(0.05)
+
+device.emit_click(uinput.KEY_ENTER)
